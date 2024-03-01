81a4c85250a6d64aff0dcfec8747fb6205e2c61c
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 00:59:03 +0100

    fin du premier exerciceXpGold du jour1

---
+++ Week2/Jour1/ExercicesXpGold/index.js
@@ -0,0 +1,5 @@
+//exerice1
+
+let sentence = ["my","favorite","color","is","blue"];
+let addittionalSentence = sentence[0] + sentence[1] + sentence[2] + sentence[3]+sentence[4];
+console.log(addittionalSentence);
