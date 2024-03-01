1ad08d56c1201897b6cb1a30bc168239125cad80
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 00:48:22 +0100

    fin du premier exercice du jour1

---
+++ Week2/Jour1/ExercicesXp/script.js
@@ -0,0 +1,187 @@
+let  aliments_preferes= "tchep";
+let repas_prefere= "diner";
+console.log("je mange du "+aliments_preferes+" au " +repas_prefere);
+
+//exercice2
+
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let nbreSerie = "2 series";
+let seriRegardé = " money heist, and the big bang theory";
+console.log("J'ai regargé "+ nbreSerie+ seriRegardé);
+myWatchedSeries[2] = "friends";
+myWatchedSeries.push("Blacklist");
+myWatchedSeries.splice(0, 1, "24 heures chronos")
+console.log(myWatchedSeries[1]);
+console.log(myWatchedSeries);
+
+//exercice3
+
+let celsius = 100;
+let fahrenheit = (celsius/5)*9;
+console.log(fahrenheit);
+
+//Exercise 4 : Guess The Answers #1
+
+
+console.log(2+3)
+// Prediction: It will output 5, because 2 and 3 are numbers
+// Actual: 5
+
+
+//Using the code below:
+
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction:
+// Actual:
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction:
+// Actual:
+
+
+
+//What will be the outcome of a + b in the first expression ?34+21=55
+    //What will be the outcome of a + b in the second expression ?2+21=23
+    //What is the value of c?non definie
+
+   // Analyse the code below, what will be the outcome?
+    console.log(3 + 4 + '5');//75
+
+
+//Exercise 5 :
+
+
+//What is the output of each of the expressions below?
+
+
+    typeof(15)
+    // Prediction:number
+    // Actual:
+
+    typeof(5.5)
+// Prediction:number
+// Actual:
+
+typeof(NaN)
+// Prediction:number
+// Actual:
+
+typeof("hello")
+// Prediction:hello
+// Actual:
+
+typeof(true)
+// Prediction:boolean
+// Actual:
+
+typeof(1 != 2)
+// Prediction:boolean
+// Actual:
+
+"hamburger" + "s"
+// Prediction:hamburgers
+// Actual:
+
+"hamburgers" - "s"
+// Prediction:NAN
+// Actual:
+
+"1" + "3"
+// Prediction:13
+// Actual:
+
+"1" - "3"
+// Prediction:-2
+// Actual:
+
+// Prediction:johnny5
+// Actual:
+
+"johnny" - 5
+// Prediction:nan
+// Actual:
+
+99 * "hello"
+// Prediction:nan
+// Actual:
+
+1 != 1
+// Prediction:false
+// Actual:
+
+1 == "1"
+// Prediction:true
+// Actual:
+
+1 === "1"
+// Prediction:false
+// Actual:
+
+
+//xercise 6 :
+    //  What is the output of each of the expressions below?
+
+
+    5 + "34"
+    // Prediction:534
+    // Actual:
+
+    5 - "4"
+// Prediction:1
+// Actual:
+
+
+10 % 5
+// Prediction:0
+// Actual:
+
+5 % 10
+// Prediction:5
+// Actual:
+
+
+"Java" + "Script"
+// Prediction:javascript
+// Actual:
+
+" " + " "
+// Prediction:
+// Actual:
+
+" " + 0
+// Prediction:0
+// Actual:
+
+true + true
+// Prediction:1
+// Actual:
+
+true + false
+// Prediction:1
+// Actual:
+
+false + true
+// Prediction:1
+// Actual:
+
+false - true
+// Prediction:-1
+// Actual:
+
+!true
+// Prediction:false
+// Actual:
+
+3 - 4
+// Prediction:-1
+// Actual:
+
+"Bob" - "bill"
+// Prediction:nan
+// Actual:
