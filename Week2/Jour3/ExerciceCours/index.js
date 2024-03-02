39f2571e2664ec29f79881488e464f2dfd0d58f3
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 23:29:46 +0100

    fin des  exerciceCours du jour3

--- Week2/Jour3/ExerciceCours/index.js
+++ Week2/Jour3/ExerciceCours/index.js
@@ -8,3 +8,28 @@ for  (let nbre=0;nbre<=15;nbre++){
     }
 
 }
+
+//Exercice 2
+
+// Liste des noms de variables
+let variables = ["chien", "Chat", 123, "oiseau", "Lion"];
+
+// Boucle for parcourant les noms des variables
+for (let i = 0; i < variables.length; i++) {
+    let variable = variables[i];
+
+    // Vérifier si l'élément n'est pas une chaîne de caractères
+    if (typeof variable !== "string") {
+        continue; // Passe à l'itération suivante si ce n'est pas une chaîne
+    }
+
+    // Vérifier si la première lettre n'est pas en majuscules
+    if (variable[0] !== variable[0].toUpperCase()) {
+        // Remplacer la première lettre par sa version en majuscules
+        variable = variable.charAt(0).toUpperCase() + variable.slice(1);
+    }
+
+    // Afficher le nom
+    console.log(variable);
+}
+
