43307478c591a8f60c3f4b4f194e98656d2b444b
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 20:36:22 +0100

    fin du deuxième exerciceXpGold du jour2

--- Week2/Jour2/ExerciceXpNinja/index.js
+++ Week2/Jour2/ExerciceXpNinja/index.js
@@ -1,3 +1,4 @@
+//exerice1
 let anneeActuel = 2024;
 let dateNaissancePersonne1 = 1950;
 let dateNaissancePersonne2 = 2001;
@@ -6,3 +7,27 @@ let ageActuelPersonne1 =  anneeActuel-dateNaissancePersonne1;
 let ageActuelPersonne2 =  anneeActuel-dateNaissancePersonne2;
 let ageMoitiePersonne2 = ((ageActuelPersonne1/2)-ageActuelPersonne2)+anneeActuel;
 console.log("C'est en "+ ageMoitiePersonne2 + " que vous aurez la moitié de l'age de la personne1 ");
+
+//exercice2
+
+// Demander au client son code postal
+let codePostal = prompt("Veuillez saisir votre code postal :");
+
+// Vérifier les conditions
+if (codePostal.length === 5 && !isNaN(codePostal) && !codePostal.includes(" ")) {
+    console.log("Succès");
+} else {
+    console.log("Erreur");
+}
+//deuxi_me méthodes avec les expressions regulière
+// Demander au client son code postal
+let cdPostal = prompt("Veuillez saisir votre code postal :");
+
+// Vérifier les conditions avec des expressions régulières
+let regex = /^\d{5}$/; // Expression régulière pour 5 chiffres seulement
+
+if (regex.test(cdPostal)) {
+    console.log("Succès");
+} else {
+    console.log("Erreur");
+}
