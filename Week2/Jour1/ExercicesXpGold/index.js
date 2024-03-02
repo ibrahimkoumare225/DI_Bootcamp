a4cc251df290693b09f13e9069346d41560eaedc
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 01:11:20 +0100

    fin du deuxième  exerciceXpGold du jour1

--- Week2/Jour1/ExercicesXpGold/index.js
+++ Week2/Jour1/ExercicesXpGold/index.js
@@ -3,3 +3,18 @@
 let sentence = ["my","favorite","color","is","blue"];
 let addittionalSentence = sentence[0] + sentence[1] + sentence[2] + sentence[3]+sentence[4];
 console.log(addittionalSentence);
+
+// Exercice 2 : Mixup
+// Étape 1 : Créer deux variables avec des chaînes de caractères
+let str1 = "mix";
+let str2 = "pod";
+
+// Étape 2 : Découper et échanger les 2 premiers caractères des deux chaînes
+let newStr1 = str2.slice(0, 2) + str1.slice(2);
+let newStr2 = str1.slice(0, 2) + str2.slice(2);
+
+// Étape 3 : Créer une troisième variable avec la concaténation des deux chaînes séparées par un espace
+let concatenatedStr = newStr1 + " " + newStr2;
+
+// Étape 4 : Afficher la nouvelle chaîne concaténée
+console.log(concatenatedStr);
