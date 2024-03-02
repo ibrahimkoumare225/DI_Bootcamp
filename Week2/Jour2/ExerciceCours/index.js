5feb5d593299347035b0dd657f77e8b9e61b3873
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 10:31:47 +0100

    fin des exrcices de cours

---
+++ Week2/Jour2/ExerciceCours/index.js
@@ -0,0 +1,22 @@
+let personne = {
+    nom_utilisateur : "ikouma",
+    motsDePasse: "Kouma010304@"
+}
+let baseDeDonnees = [personne];
+
+let newwsfriend = [
+    {
+        username : "booba",
+        timeline: 500
+    },
+    {
+        username : "ninho",
+        timeline: 800
+    },
+    {
+        username : "damso",
+        timeline: 700
+    }
+]
+console.log(personne);
+console.log(newwsfriend);
