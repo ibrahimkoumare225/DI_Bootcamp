0108507e74de962d385f38e63c44aea7bf6d21a2
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 12:02:36 +0100

    fin du premier exerciceXpNinja du jour2

---
+++ Week2/Jour2/ExerciceXpNinja/index.js
@@ -0,0 +1,8 @@
+let anneeActuel = 2024;
+let dateNaissancePersonne1 = 1950;
+let dateNaissancePersonne2 = 2001;
+
+let ageActuelPersonne1 =  anneeActuel-dateNaissancePersonne1;
+let ageActuelPersonne2 =  anneeActuel-dateNaissancePersonne2;
+let ageMoitiePersonne2 = ((ageActuelPersonne1/2)-ageActuelPersonne2)+anneeActuel;
+console.log("C'est en "+ ageMoitiePersonne2 + " que vous aurez la moitié de l'age de la personne1 ");
