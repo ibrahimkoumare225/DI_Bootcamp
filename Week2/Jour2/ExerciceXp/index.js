0c4ccfde20b71a916b8c4e187ac81ee78cde2ad0
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 10:52:54 +0100

    fin des exerciceXp du jour2

--- Week2/Jour2/ExerciceXp/index.js
+++ Week2/Jour2/ExerciceXp/index.js
@@ -36,3 +36,16 @@ if (nbreSaisir % 2 ==0 ){
 else {
     console.log(nbreSaisir+" est un nombre impair");
 }
+// Exercice 4 : Discussion De Groupe
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+// Vérifier le nombre d'utilisateurs en ligne
+if (users.length === 0) {
+    console.log("Personne n'est en ligne.");
+} else if (users.length === 1) {
+    console.log(users[0] + " est en ligne.");
+} else if (users.length === 2) {
+    console.log(users[0] + " et " + users[1] + " sont en ligne.");
+} else {
+    let additionalUsers = users.length - 2;
+    console.log(users[0] + ", " + users[1] + " et " + additionalUsers + " autres sont en ligne.");
+}
