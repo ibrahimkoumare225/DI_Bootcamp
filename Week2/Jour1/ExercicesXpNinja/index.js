a3a62db085a35e95b6f8c22834bfbaff55016ff8
Koumare <ibrahimkoumare5@gmail.com>
2024-03-02 01:48:54 +0100

    fin du premier exerciceXpNinja du jour1

---
+++ Week2/Jour1/ExercicesXpNinja/index.js
@@ -0,0 +1,11 @@
+//Exercice 1 : prediction + explication
+5 >= 1 // Vrai. (5 est supérieur ou égal à 1)
+0 === 1 // faux car 0 est inférieur a 1
+4 <= 1// faux car 4 est inférieur 1
+1 != 1// faux car 1 est égale a 1
+"A" > "B"//faux le binaire de A est inférieur a celui de B
+"B" < "C"// vrai le binaire de B est inférieur a celui de C
+"a" > "A"//vrai le binaire de a est inférieur a celui de A
+"b" < "A"//faux le binaire de b est supérieur a celui de A
+true === false//faux true est différent de false
+true != true//faux true est egale true
