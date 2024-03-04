0d8b25af893b6413876bfcf587e5597b268df11b
Koumare <ibrahimkoumare5@gmail.com>
2024-03-04 15:56:03 +0100

    Soumission de formulaire

--- Week2/Jour4/AZERTYPE/Scripts/script.js
+++ Week2/Jour4/AZERTYPE/Scripts/script.js
@@ -8,13 +8,28 @@ function afficherResultat(score, nbMotsProposes) {
     let aff = document.querySelector(".zoneScore span")
     aff.innerHTML=html
 }
-
+/**
+ * Cette fonction affiche une proposition, que le joueur devra recopier,
+ * dans la zone "zoneProposition"
+ * @param {string} proposition : la proposition à afficher
+ */
 function afficherProposition(proposition){
     let zoneProposition = document.querySelector(".zoneProposition");
     zoneProposition.innerText = proposition
 }
+/**
+ * Cette fonction construit et affiche l'email.
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score.
+ */
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}+?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je vais de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
 //fonction principale
 function lancerJeu() {
+    initAddEventListenerPopup()
     let score = 0
     let i = 0
     let listeProposition = listeMots
@@ -51,6 +66,19 @@ function lancerJeu() {
             afficherProposition( listeProposition[i])
         })
     }
+    let form = document.querySelector("form")
+    form.addEventListener("submit",(event)=> {
+        event.preventDefault()
+
+        let baliseNom = document.getElementById("nom")
+        let nom = baliseNom.value
+
+        let baliseEmail = document.getElementById("email")
+        let email = baliseEmail.value
+
+        let scoreEmail = `${score}/${i}`
+        afficherEmail(nom,email,scoreEmail)
+    })
    afficherResultat(score,i)
 }
 
